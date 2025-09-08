a0cc30bc1ce3fa7ed762cdaccc39312944136ea9
HassanFarid5266 <hassanbaba8142@gmail.com>
2025-09-08 11:13:21 +0500

    fixes

--- src/views/RefundsPolicy.vue
+++ src/views/RefundsPolicy.vue
@@ -1,5 +1,76 @@
 <template>
-  
+    <section class="page-head">
+        <div class="boxed">
+            <span class="page-badge">About</span>
+            <h1 class="page-title">Refunds policy</h1>
+            <p class="page-desc">
+                - Thank you for choosing NexTash. We strive to provide you with
+                the best products and services, but we understand that there may
+                be instances where you need to request a refund. Please review our
+                Refunds Policy below for information on how we handle refund
+                requests.
+            </p>
+        </div>
+    </section>
+    <main class="boxed content">
+        <h2 class="heading dotted-square">Refund Eligibility</h2>
+        <p class="description">
+            NexTash offers refunds on products and services within a specified
+            timeframe from the date of purchase. The eligibility period may vary
+            based on the type of product or service.
+        </p>
+        <h2 class="heading dotted-square">Refund Requests</h2>
+        <p class="description">
+            To initiate a refund request, please contact our customer support
+            team at <a href="mailto:support@nextash.com" class="link">support@nextash.com</a>
+            within the specified
+            eligibility period. Provide your order number, a detailed reason for
+            the refund request, and any supporting documentation, if necessary.
+        </p>
+        <h2 class="heading dotted-square">Non-Refundable Items</h2>
+        <p class="description">
+            Certain products or services may not be eligible for a refund. These
+            include, but are not limited to, digital downloads, customized
+            products, and services that have already been rendered.
+        </p>
+        <h2 class="heading dotted-square">Processing Time</h2>
+        <p class="description">
+            Once your refund request is received, we will review it and notify
+            you of the approval or rejection of your refund. The processing time
+            may vary, but we strive to complete the review within 15 days business
+            days.
+        </p>
+        <h2 class="heading dotted-square">Refund Method</h2>
+        <p class="description">
+            Approved refunds will be processed using the original method of
+            payment. Please allow 15 business days for the refund to be
+            reflected in your account.
+        </p>
+        <h2 class="heading dotted-square">Cancelled Orders</h2>
+        <p class="description">
+            If you cancel an order before it is processed, a full refund will be
+            issued. However, if the order has already been processed, it will be
+            treated as a standard refund request.
+        </p>
+        <h2 class="heading dotted-square">Contact Information</h2>
+        <p class="description">
+            For any questions regarding our Refunds Policy, please contact us at
+            <a href="mailto:support@nextash.com" class="link">support@nextash.com</a>.
+        </p>
+        <h2 class="heading dotted-square">Policy Updates</h2>
+        <p class="description">
+            NexTash reserves the right to update or modify this <a href="/refunds-policy/" class="link">Refunds
+                Policy</a>
+            at any time. Please check our website for the latest version.
+        </p>
+        <p class="description">
+            By making a purchase with NexTash, you acknowledge and agree to
+            comply with our Refunds Policy. We appreciate your understanding and
+            cooperation.
+        </p>
+        <p class="description">Thank you for choosing NexTash!</p>
+    </main>
+    <br><br><br><br><br>
 </template>
 
 <script setup>
