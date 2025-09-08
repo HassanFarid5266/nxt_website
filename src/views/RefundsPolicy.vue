9e2d2b11fde98b6b95a0c7a2f770ae7f13ed40d4
HassanFarid5266 <hassanbaba8142@gmail.com>
2025-09-08 11:07:54 +0500

    polices page

--- src/views/RefundsPolicy.vue
+++ src/views/RefundsPolicy.vue
@@ -1,14 +1,6 @@
 <template>
-  <main>
-    <div class="container">
-      <h1>Refunds Policy</h1>
-      <p>This is the Refunds Policy page. Content will be converted from the original refunds-policy.html</p>
-    </div>
-  </main>
+  
 </template>
 
-<script>
-export default {
-  name: 'RefundsPolicy'
-}
+<script setup>
 </script>
